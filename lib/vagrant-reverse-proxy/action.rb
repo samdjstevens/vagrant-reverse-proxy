d4836bbe8ba3c9e5c4c2bd4d1746d15f7fdb280e
Peter Bex <peter@codeyellow.nl>
2016-03-22 16:41:15 +0100

    Do not discard host config from other boxes.
    I misunderstood what Vagrant considers to be an "environment";
    apparently it includes only the box(es?) corresponding to the current
    Vagrantfile.  So now we keep track of whether we're adding or removing
    the box, and add markers to the file so we're able to remove the current
    box's config block when we're halting the VM.

--- lib/vagrant-reverse-proxy/action.rb
+++ lib/vagrant-reverse-proxy/action.rb
@@ -9,15 +9,15 @@ module VagrantPlugins
       # machine; we always write a complete config with all machines.
       def self.add_machine
         Vagrant::Action::Builder.new.tap do |builder|
-          builder.use ConfigValidate
-          builder.use WriteNginxConfig
+          builder.use(ConfigValidate)
+          builder.use(WriteNginxConfig, :add)
         end
       end
 
       def self.remove_machine
         Vagrant::Action::Builder.new.tap do |builder|
-          builder.use ConfigValidate
-          builder.use WriteNginxConfig
+          builder.use(ConfigValidate)
+          builder.use(WriteNginxConfig, :remove)
         end
       end
     end
--- lib/vagrant-reverse-proxy/action/write_nginx_config.rb
+++ lib/vagrant-reverse-proxy/action/write_nginx_config.rb
@@ -2,11 +2,12 @@ module VagrantPlugins
   module ReverseProxy
     module Action
       class WriteNginxConfig
-        def initialize(app, env)
+        def initialize(app, env, action)
           @app = app
           @global_env = env[:machine].env
           @provider = env[:machine].provider_name
           @config = @global_env.vagrantfile.config
+          @action = action
         end
 
         def call(env)
@@ -25,9 +26,28 @@ module VagrantPlugins
 
           env[:ui].info('Updating nginx configuration. Administrator privileges will be required...')
 
-          File.open(tmp_file, 'w') do |f|
-            get_machines().each do |m|
-              f.write(server_block(m))
+          machines = get_machines()
+          # This code is so stupid
+          File.open(nginx_site, 'r') do |old|
+            File.open(tmp_file, 'w') do |new|
+              sm = machines.map {|m| start_marker(m) }
+              em = machines.map {|m| end_marker(m) }
+
+              while ln = old.gets() do
+                if sm.member?(ln.chomp)
+                  until !ln || em.member?(ln.chomp) do
+                    ln = old.gets()
+                  end
+                else
+                  new.puts(ln)
+                end
+              end
+
+              if @action == :add
+                machines.each do |m|
+                  new.write(start_marker(m)+"\n"+server_block(m)+end_marker(m)+"\n")
+                end
+              end
             end
           end
 
@@ -57,14 +77,21 @@ EOF
           end.join("\n")
         end
 
+        def start_marker(m)
+          "# BEGIN #{m.id} #"
+        end
+
+        def end_marker(m)
+          "# END #{m.id} #"
+        end
+
         # Machine-finding code stolen from vagrant-hostmanager :)
         def get_machines()
           # Collect only machines that exist for the current provider
           @global_env.active_machines.collect do |name, provider|
             if provider == @provider
               begin
-                m = @global_env.machine(name, @provider)
-                m.state.id == :running ? m : nil
+                @global_env.machine(name, @provider)
               rescue Vagrant::Errors::MachineNotFound
                 nil #ignore
               end
