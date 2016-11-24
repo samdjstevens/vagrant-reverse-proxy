9fc6ef05702765bcb02ffbd6814bc90b31ad6a66
Sam Stevens <sam@ticketline.co.uk>
2016-11-24 11:01:54 +0000

    remove I.P that snuck in

--- lib/vagrant-reverse-proxy/action/write_nginx_config.rb
+++ lib/vagrant-reverse-proxy/action/write_nginx_config.rb
@@ -96,7 +96,6 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_pass http://192.168.33.100/;
         proxy_pass http://#{ip}#{port_suffix}/;
     }
 }
